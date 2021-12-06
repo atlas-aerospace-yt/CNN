0d96dac4ebee1d38986f637e9d49d3de679561fe
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2021-12-06 22:17:18 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,32 @@ import numpy as np
 
 if __name__ == "__main__":
 
-    input = np.random.uniform(-1, 1, (8, 8))
-    model = nn(input, 3, 6)
+    input = np.zeros((8,8))
 
-    model.forward(input)
+    input_fat = ([[1, 0, 0, 1, 1, 0, 0, 1],
+                       [1, 0, 1, 1, 1, 1, 0, 1],
+                       [0, 1, 0, 1, 1, 0, 1, 0],
+                       [0, 1, 1, 1, 1, 1, 1, 0],
+                       [0, 1, 0, 0, 0, 0, 1, 0],
+                       [0, 0, 1, 1, 1, 1, 0, 0],
+                       [1, 1, 0, 1, 1, 0, 1, 1],
+                       [1, 0, 0, 0, 0, 0, 0, 1]])
+
+    input_thin = np.matrix([[0, 0, 1, 0, 0, 1, 0, 0],
+                       [0, 0, 1, 1, 1, 1, 0, 0],
+                       [0, 0, 1, 1, 1, 1, 0, 0],
+                       [0, 1, 0, 1, 1, 0, 1, 0],
+                       [1, 0, 1, 1, 1, 1, 0, 1],
+                       [0, 0, 1, 1, 1, 1, 0, 0],
+                       [0, 1, 1, 0, 0, 1, 1, 0],
+                       [0, 1, 0, 0, 0, 0, 1, 0]])
+
+    model = nn(input, 3, 2)
+
+    model.forward(input_fat)
+
+    for x in range(0, 100):
+        model.backward(input_fat, 2)
+
+    for x in range(0, 100):
+        model.backward(input_thin, 1)
