1fc73962c7d07a93817cb5b0b1654a5e5cf24ac0
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2021-12-10 00:19:34 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from CNN import NeuralNetwork as nn
 import numpy as np
+from PIL import Image
 
 if __name__ == "__main__":
 
@@ -23,12 +24,18 @@ if __name__ == "__main__":
                        [0, 1, 1, 0, 0, 1, 1, 0],
                        [0, 1, 0, 0, 0, 0, 1, 0]])
 
-    model = nn(input, 3, 2)
+    model = nn(input_fat, 10, 2)
 
-    model.forward(input_fat)
-
-    for x in range(0, 1000):
-        model.backward(([[0, 1]]), input_fat)
+    #image = (Image.fromarray(np.array(input_thin) * 255)).resize((1000,1000), Image.ANTIALIAS)
+    #image.show()
+    #prediction = model.forward(input_fat)
 
     #for x in range(0, 100):
-        #model.backward(input_thin, 1)
+    model.backward(([[0, 1]]), input_fat)
+
+    #for x in range(0, 100):
+        #model.backward(([[1, 0]]), input_thin)
+
+    #prediction = model.forward(input_fat)
+
+    #print(prediction)
