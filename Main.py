b8c9c9f621aec14a82872ad9cad4144d2f79b22f
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2021-12-12 23:21:28 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,41 +1,64 @@
+import matplotlib.pyplot as plt
 from CNN import NeuralNetwork as nn
 import numpy as np
 from PIL import Image
 
 if __name__ == "__main__":
 
-    input = np.zeros((8,8))
+    input = np.zeros((1,2))
 
-    input_fat =      ([[1, 0, 0, 1, 1, 0, 0, 1],
-                       [1, 0, 1, 1, 1, 1, 0, 1],
-                       [0, 1, 0, 1, 1, 0, 1, 0],
-                       [0, 1, 1, 1, 1, 1, 1, 0],
-                       [0, 1, 0, 0, 0, 0, 1, 0],
-                       [0, 0, 1, 1, 1, 1, 0, 0],
-                       [1, 1, 0, 1, 1, 0, 1, 1],
-                       [1, 0, 0, 0, 0, 0, 0, 1]])
+    #input_fat =      ([[1, 0, 0, 1, 1, 0, 0, 1],
+    #                   [1, 0, 1, 1, 1, 1, 0, 1],
+    #                   [0, 1, 0, 1, 1, 0, 1, 0],
+    #                   [0, 1, 1, 1, 1, 1, 1, 0],
+    #                   [0, 1, 0, 0, 0, 0, 1, 0],
+    #                   [0, 0, 1, 1, 1, 1, 0, 0],
+    #                   [1, 1, 0, 1, 1, 0, 1, 1],
+    #                   [1, 0, 0, 0, 0, 0, 0, 1]])
 
-    input_thin =     ([[0, 0, 1, 0, 0, 1, 0, 0],
-                       [0, 0, 1, 1, 1, 1, 0, 0],
-                       [0, 0, 1, 1, 1, 1, 0, 0],
-                       [0, 1, 0, 1, 1, 0, 1, 0],
-                       [1, 0, 1, 1, 1, 1, 0, 1],
-                       [0, 0, 1, 1, 1, 1, 0, 0],
-                       [0, 1, 1, 0, 0, 1, 1, 0],
-                       [0, 1, 0, 0, 0, 0, 1, 0]])
+    #input_fat = input_fat.astype('float32')
 
-    model = nn(input_fat, 10, 2)
+    #input_thin =     ([[0, 0, 1, 0, 0, 1, 0, 0],
+    #                   [0, 0, 1, 1, 1, 1, 0, 0],
+    #                   [0, 0, 1, 1, 1, 1, 0, 0],
+    #                   [0, 1, 0, 1, 1, 0, 1, 0],
+    #                   [1, 0, 1, 1, 1, 1, 0, 1],
+    #                   [0, 0, 1, 1, 1, 1, 0, 0],
+    #                   [0, 1, 1, 0, 0, 1, 1, 0],
+    #                   [0, 1, 0, 0, 0, 0, 1, 0]])
+
+    #input_thin = input_thin.astype('float32')
+
+    model = nn(input, 1, 1)
 
     #image = (Image.fromarray(np.array(input_thin) * 255)).resize((1000,1000), Image.ANTIALIAS)
     #image.show()
     #prediction = model.forward(input_fat)
 
     #for x in range(0, 100):
-    model.backward(([[0, 1]]), input_fat)
+    #model.backward(([[0, 1]]), input_fat)
 
-    #for x in range(0, 100):
-        #model.backward(([[1, 0]]), input_thin)
+    cost = []
 
-    #prediction = model.forward(input_fat)
+    for x in range(0, 1000):
 
-    #print(prediction)
+        model.backward(([[0]]), ([[0],[1]]))
+        model.backward(([[0]]), ([[0],[0]]))
+        model.backward(([[1]]), ([[1],[0]]))
+        model.backward(([[1]]), ([[1],[1]]))
+
+        cost.append(model.MSE)
+
+    list = []
+
+    for x in range(0,len(cost)):
+
+        list.append(x)
+
+    plt.plot(list, cost)
+    plt.show()
+
+    print(model.bias)
+    print(model.weights)
+    print(model.forward(([[1],[0]])))
+    print(model.forward(([[0],[1]])))
