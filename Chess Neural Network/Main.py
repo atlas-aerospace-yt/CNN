0711f5de88a82e59157c61f8b1cc8702b6d92b9c
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2022-08-01 19:28:09 +0100

    Error

---
+++ Chess Neural Network/Main.py
@@ -0,0 +1,8 @@
+import Chess
+
+## TODO: Add promotion to the CNN
+## TODO: Finish data organiser to train CNN
+ 
+if __name__ == "__main__":
+
+    Chess.startup.start()
--- ChessEngine/Main.py
+++
@@ -1,5 +0,0 @@
-import Chess_v1 as Chess
-
-if __name__ == "__main__":
-
-    Chess.startup.start()
